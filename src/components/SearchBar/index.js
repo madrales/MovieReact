5a6298bf1f8e5484a1a670f17e680379c2781853
Mason Adrales <masonadrales@Masons-MacBook-Pro-4.local>
2021-08-09 17:04:58 -0500

    Made Home and Search bar into Components rather than functional components

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect, useRef} from 'react';
-
+// import React, {useState, useEffect, useRef} from 'react';
+import React, {Component} from 'react';
 //styles
 import {Wrapper, Content} from './SearchBar.styles';
 
@@ -7,36 +7,75 @@ import {Wrapper, Content} from './SearchBar.styles';
 import searchIcon from '../../images/search-icon.svg';
 import PropTypes from 'prop-types';
 
-const SearchBar = ({setSearchTerm}) => {
-    const [state, setState] = useState('');
+class SearchBar extends Component{
 
-    const initial = useRef(true);
+    state = {value: ''};
+    timeout = null;
 
-    useEffect(() => {
-        if (initial.current){
-            initial.current = false;
-            return;
+    componentDidUpdate(_prevProps, prevState){
+        if (this.state.value !== prevState.value){
+            const { setSearchTerm } = this.props;
+
+            clearTimeout(this.timeout);
+
+            this.timeout = setTimeout(() => {
+                const {value} = this.state;
+                setSearchTerm(value);
+            }, 500);
         }
-        const timer = setTimeout(() => {
-            setSearchTerm(state);
-        }, 500)
-        return () => clearTimeout(timer)
-    }, [setSearchTerm, state])
+    }
 
-    return (
-        <Wrapper>
-            <Content>
-                <img src={searchIcon} alt="search-icon"/>
-                <input
-                    type='text'
-                    placeholder='Search Movie'
-                    onChange={event => setState(event.currentTarget.value)}
-                    value={state}
-                />
-            </Content>
-        </Wrapper>
-    )
+    render() {
+        const {value} = this.state;
+        return (
+            <Wrapper>
+                <Content>
+                    <img src={searchIcon} alt="search-icon"/>
+                    <input
+                        type='text'
+                        placeholder='Search Movie'
+                        onChange={event => this.setState({value: event.currentTarget.value})}
+                        value={this.state.value}
+                    />
+                </Content>
+            </Wrapper>
+        )
+    }
 };
+//         return () => clearTimeout(timer)
+//     }, [setSearchTerm, state])
+// };
+
+// const SearchBar = ({setSearchTerm}) => {
+//     const [state, setState] = useState('');
+
+//     const initial = useRef(true);
+
+//     useEffect(() => {
+//         if (initial.current){
+//             initial.current = false;
+//             return;
+//         }
+//         const timer = setTimeout(() => {
+//             setSearchTerm(state);
+//         }, 500)
+//         return () => clearTimeout(timer)
+//     }, [setSearchTerm, state])
+
+//     return (
+//         <Wrapper>
+//             <Content>
+//                 <img src={searchIcon} alt="search-icon"/>
+//                 <input
+//                     type='text'
+//                     placeholder='Search Movie'
+//                     onChange={event => setState(event.currentTarget.value)}
+//                     value={state}
+//                 />
+//             </Content>
+//         </Wrapper>
+//     )
+// };
 
 SearchBar.propTypes = {
     callback: PropTypes.func
