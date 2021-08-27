40860c3fd191376ba8e82ff351647d57fb2a354b
Mason Adrales <masonadrales@mwc-107001.dhcp.missouri.edu>
2021-08-27 10:47:58 -0500

    commit before updating

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,6 +20,43 @@ const initialState = {
     total_pages: 0,
     total_results: 0
 };
+const Home = () => {
+    const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore} = useHomeFetch();
+    console.log(state);
+    if(error) return <div>Something went wrong...</div>
+    return (
+        <>
+            {!searchTerm && state.results[0] ? (
+                <HeroImage 
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
+                /> 
+             ) : null}
+             <SearchBar setSearchTerm={setSearchTerm}/>
+             <Grid header = {searchTerm ? 'Search Results' : 'Popular Movies'}>
+                 {state.results.map(movie => (
+                     <Thumb
+                        key={movie.id}
+                        clickable
+                        image ={
+                            movie.poster_path
+                            ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                            : NoImage
+                        }
+                        movieId={movie.id}
+                     />
+                 ))}
+             </Grid>
+             {loading && <Spinner/>}
+             {state.page < state.total_pages && !loading && (
+                 <Button text="Load More" callback={() => setIsLoadingMore(true)}/>
+             )}
+        </>
+    );
+};
+
+export default Home;
 
 // class Home extends Component{
 //     state = {
@@ -98,41 +135,3 @@ const initialState = {
 //         );
 //     };
 // };
-
-const Home = () => {
-    const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore} = useHomeFetch();
-    console.log(state);
-    if(error) return <div>Something went wrong...</div>
-    return (
-        <>
-            {!searchTerm && state.results[0] ? (
-                <HeroImage 
-                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-                    title={state.results[0].original_title}
-                    text={state.results[0].overview}
-                /> 
-             ) : null}
-             <SearchBar setSearchTerm={setSearchTerm}/>
-             <Grid header = {searchTerm ? 'Search Results' : 'Popular Movies'}>
-                 {state.results.map(movie => (
-                     <Thumb
-                        key={movie.id}
-                        clickable
-                        image ={
-                            movie.poster_path
-                            ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
-                            : NoImage
-                        }
-                        movieId={movie.id}
-                     />
-                 ))}
-             </Grid>
-             {loading && <Spinner/>}
-             {state.page < state.total_pages && !loading && (
-                 <Button text="Load More" callback={() => setIsLoadingMore(true)}/>
-             )}
-        </>
-    );
-};
-
-export default Home;
